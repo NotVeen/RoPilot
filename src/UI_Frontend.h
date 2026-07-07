c4cb3e006276bf01bbeab38d1830b0b06fea6998
Mochammad Alvin Avisena <alvin.avisena2502@gmail.com>
2026-07-07 12:07:34 +0700

    Add darker custom scrollbar CSS

--- src/UI_Frontend.h
+++ src/UI_Frontend.h
@@ -39,6 +39,23 @@ const char* HTML_CONTENT = R"HTML(
             user-select: none; /* Disable text selection */
         }
         
+        /* Custom Scrollbar */
+        ::-webkit-scrollbar {
+            width: 8px;
+            height: 8px;
+        }
+        ::-webkit-scrollbar-track {
+            background: rgba(0, 0, 0, 0.2);
+            border-radius: 4px;
+        }
+        ::-webkit-scrollbar-thumb {
+            background: rgba(255, 255, 255, 0.1);
+            border-radius: 4px;
+        }
+        ::-webkit-scrollbar-thumb:hover {
+            background: rgba(255, 255, 255, 0.2);
+        }
+
         input {
             user-select: auto;
         }
