840d937e6960ed9ab6299602a3e29c8e92ab68eb
Mochammad Alvin Avisena <alvin.avisena2502@gmail.com>
2026-07-07 12:10:07 +0700

    Make scrollbar darker

--- src/UI_Frontend.h
+++ src/UI_Frontend.h
@@ -45,15 +45,14 @@ const char* HTML_CONTENT = R"HTML(
             height: 8px;
         }
         ::-webkit-scrollbar-track {
-            background: rgba(0, 0, 0, 0.2);
-            border-radius: 4px;
+            background: transparent;
         }
         ::-webkit-scrollbar-thumb {
-            background: rgba(255, 255, 255, 0.1);
+            background: rgba(255, 255, 255, 0.05);
             border-radius: 4px;
         }
         ::-webkit-scrollbar-thumb:hover {
-            background: rgba(255, 255, 255, 0.2);
+            background: rgba(255, 255, 255, 0.1);
         }
 
         input {
